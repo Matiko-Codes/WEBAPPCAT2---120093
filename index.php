5b0d8694db282c06fa0aed01cd1d7ec26940288f
Matiko-Codes <mmatiko243@gmail.com>
2023-11-16 08:35:16 +0300

    Added the Index Form and styled it with CSS

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Author Details Submission</title>
+</head>
+<body>
+
+    <h2>Author Details Submission</h2>
+
+    <form action="process_form.php" method="post">
+        <!-- AuthorId -->
+        <label for="authorId">Author ID:</label>
+        <input type="text" id="authorId" name="authorId" required>
+        <br>
+
+        <!-- AuthorFullName -->
+        <label for="authorFullName">Author Full Name:</label>
+        <input type="text" id="authorFullName" name="authorFullName" required>
+        <br>
+
+        <!-- AuthorEmail -->
+        <label for="authorEmail">Author Email:</label>
+        <input type="email" id="authorEmail" name="authorEmail" required>
+        <br>
+
+        <!-- AuthorAddress -->
+        <label for="authorAddress">Author Address:</label>
+        <input type="text" id="authorAddress" name="authorAddress">
+        <br>
+
+        <!-- AuthorBiography -->
+        <label for="authorBiography">Author Biography:</label>
+        <textarea id="authorBiography" name="authorBiography" rows="4" cols="50"></textarea>
+        <br>
+
+        <!-- AuthorDateOfBirth -->
+        <label for="authorDateOfBirth">Date of Birth:</label>
+        <input type="date" id="authorDateOfBirth" name="authorDateOfBirth">
+        <br>
+
+        <!-- AuthorSuspended -->
+        <label for="authorSuspended">Suspended:</label>
+        <input type="checkbox" id="authorSuspended" name="authorSuspended">
+        <br>
+
+        <input type="submit" value="Submit">
+    </form>
+
+</body>
+</html>
